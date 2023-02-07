9b4cbe894ac7cad71cafe13d963bc516b8a4b13c
Justin <ileicathat@gmail.com>
2023-02-07 23:31:01 +0000

    program to print some text

---
+++ helloworld.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Script: Ops 201 Class 02 Ops Challenge Solution
+# Author: Justin R. Dotson
+# Date of latest revision: 02-07-23
+# Purpose: Print a string to the terminal
+
+
+# Main
+echo Hello,
+echo 
+echo This is my first Bash Program.
+echo 
+echo Thank you for reading!
+# End
