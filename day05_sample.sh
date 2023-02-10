f7073c1cb4c8df4f60fdce96f2a4148a54c2c775
Justin Dotson <ileicathat@gmail.com>
2023-02-10 14:59:41 -0800

    up

---
+++ day05_sample.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#!usr/bin/env bash
+
+# Script: Ops 201 Class 04 Ops Challenge Solution
+# Author: Justin R. Dotson
+# Date of latest revision: 02-09-23
+# Purpose: Write a script that:
+# Creates four directories: dir1, dir2, dir3, dir4
+# Put the **names** of the four directories in an array
+# References the array variable to create a new .txt file in each directory
+
+# Referenced : https://stackoverflow.com/questions/20243467/write-bash-array-to-file-with-newlines
+# Referenced : https://tiswww.case.edu/php/chet/bash/bashref.html#Special-Parameters
+
+# Here is how a read command works to get input from user
+
+# Main
+echo "please enter some text for me: "
+read input_var
+echo "You wrote:"
+echo $input_var
