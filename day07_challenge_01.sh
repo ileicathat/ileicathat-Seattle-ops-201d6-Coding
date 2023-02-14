f8d6f76d535c77080278674fb0c23ab015ef25d4
Justin <ileicathat@gmail.com>
2023-02-14 22:58:26 +0000

    up

---
+++ day07_challenge_01.sh
@@ -0,0 +1,14 @@
+# Script: Ops 201 Class 07 Ops Challenge
+# Author: Justin R. Dotson
+# Date of latest revision: 02-14-23
+# Purpose: Ops Challenge - System Information
+        # Overview write a script that outputs a report about current system CPU and RAM performance of a Ubuntu Linux computer.
+        # Conditionals allow your script to test whether a situation is true and then proceed accordingly.        # 
+
+        # Objectives
+            # Uses lshw to display system information to the screen about the following components:
+            # Uses grep to remove irrelevant information from the lshw output
+            # Add text to the output clearly indicating which component (such as CPU, RAM, etc.) the script is returning information about
+            # Runs as Root; you may execute the shell script with sudo or as Root
+
+# Main
