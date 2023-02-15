43633b06fb60038e7d3bcbb038357d616d0ee9de
Justin <ileicathat@gmail.com>
2023-02-15 00:30:42 +0000

    up

--- day07_challenge_01.sh
+++ day07_challenge_01.sh
@@ -10,5 +10,50 @@
             # Uses grep to remove irrelevant information from the lshw output
             # Add text to the output clearly indicating which component (such as CPU, RAM, etc.) the script is returning information about
             # Runs as Root; you may execute the shell script with sudo or as Root
+        # Notes:
+            #CPU Var set
+            #CPU=$(complex_command)
 
 # Main
+
+# Init Var
+    #array=() 
+    CPU=()
+    display=()
+    network=()
+    RAM=()
+
+echo "We are going to check out your computer."
+echo ""
+sleep 1
+echo "Parsing, please wait."
+echo ""
+echo ""
+#CPU nfo
+CPU=$(sudo lshw | grep -A 6 "*-cpu" | grep -v "capabilities")
+
+#Display nfo
+display=$(sudo lshw | grep -A 14 "*-display")
+
+#Network Nfo
+network=$(sudo lshw | grep -A 20 "*-network")
+
+#RAM
+RAM=$(sudo lshw | grep -A 4 "*-memory")
+echo "Your CPU Info is:" $CPU
+echo ""
+echo ""
+sleep 2
+echo "Your Display Info is:" $display
+echo ""
+echo ""
+sleep 2
+echo "Your Network Specs are:" $network
+echo ""
+echo ""
+sleep 2
+echo "Your RAM is:" $RAM 
+echo ""
+echo ""
+sleep 1
+echo "Not bad!"
