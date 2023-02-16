b97212ffcf711e6f2887988ba8e7b1a7607b0b71
Justin <ileicathat@gmail.com>
2023-02-16 23:12:59 +0000

    psise upload

--- Ops201d6 Challenge PS_ISE.ps1
+++ Ops201d6 Challenge PS_ISE.ps1
@@ -11,6 +11,13 @@ Get-EventLog -LogName System -EntryType Error > errors.txt
 
 #Print to the screen all events with ID of 16 from the System event log.
 
+Get-EventLog -LogName System -InstanceId 16
+
 #Print to the screen the most recent 20 entries from the System event log.’
 
-#Print to the screen all sources of the 500 most recent entries in the System event log. Ensure that the full lines are displayed (get rid of the … and show the entire text).
+Get-EventLog -LogName System -Newest 20
+
+#Print to the screen all sources of the 500 most recent entries in the System event log. Ensure that the full lines are 
+#displayed (get rid of the … and show the entire text).
+
+Get-EventLog -LogName System -Newest 500 | format-list
